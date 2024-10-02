c07fb2db756228d73fa316d9a1655711abb92c10
Syed Umer Ali <sumer@digital-dividend.com>
2024-10-03 00:46:47 +0500

    verification code screen in process

--- src/screens/Verification/index.js
+++ src/screens/Verification/index.js
@@ -1,17 +1,66 @@
 import React, {useState, useRef} from 'react';
-import {ScrollView, Text, View} from 'react-native';
+import {I18nManager, ScrollView, Text, TextInput, View} from 'react-native';
 import en from '../../translations/en.json';
 import Header from '../../components/Header';
 import styles from './styles';
 import SubmitButton from '../../components/Buttons/SubmitButton';
+import {
+  CodeField,
+  Cursor,
+  useBlurOnFulfill,
+  useClearByFocusCell,
+} from 'react-native-confirmation-code-field';
 
 const Verification = () => {
   const scrollViewRef = useRef(null);
+  const CELL_COUNT = 6;
+  const [value, setValue] = useState();
+  const codeFieldRef = useBlurOnFulfill({value, cellCount: CELL_COUNT});
+  const [codeFieldProps, getCellOnLayoutHandler] = useClearByFocusCell({
+    value,
+    setValue,
+  });
 
   const renderInputFields = () => {
     return (
       <View style={styles.mainInputContainer}>
-        <Text style={styles.headingTextStyle}>{en.verificationScreen.heading}</Text>
+        <Text style={styles.headingTextStyle}>
+          {en.verificationScreen.heading}
+        </Text>
+
+        <CodeField
+          ref={codeFieldRef}
+          {...codeFieldProps}
+          value={value}
+          onChangeText={setValue}
+          cellCount={CELL_COUNT}
+          rootStyle={[
+            styles.codeFieldRoot,
+            I18nManager.isRTL && {flexDirection: 'row-reverse'},
+          ]}
+          keyboardType="number-pad"
+          textContentType="oneTimeCode"
+          autoFocus
+          selectTextOnFocus={true}
+          onSubmitEditing={() => handleCodeVerification()}
+          textInputStyle={{textAlign: 'left'}}
+          renderCell={({index, symbol, isFocused}) => {
+            if (symbol) {
+              return (
+                <View style={styles.containerCellView}>
+                  <Text
+                    allowFontScaling={false}
+                    key={index}
+                    style={[symbol && styles.focusCell]}
+                    onLayout={getCellOnLayoutHandler(index)}>
+                    {symbol || (isFocused ? <Cursor /> : null)}
+                  </Text>
+                </View>
+              );
+            }
+            return <View style={styles.defaultCellContainer}></View>
+          }}
+        />
 
         <SubmitButton
           title={en.verificationScreen.submitBtnText}
--- src/screens/Verification/styles.js
+++ src/screens/Verification/styles.js
@@ -1,15 +1,16 @@
 import {StyleSheet} from 'react-native';
 import {Colors, Metrics} from '../../theme';
+import {memo} from 'react';
 
 const styles = StyleSheet.create({
   mainInputContainer: {
-    paddingVertical: Metrics.screenHeight * 0.05,
+    paddingVertical: Metrics.screenHeight * 0.02,
   },
 
   headingTextStyle: {
     color: Colors.lightMode.black2,
     fontWeight: '500',
-    fontSize: Metrics.ratio(20),
+    fontSize: Metrics.ratio(24),
     width: Metrics.screenWidth * 0.6,
   },
 
@@ -25,6 +26,35 @@ const styles = StyleSheet.create({
   submitButtonStyle: {
     width: Metrics.screenWidth * 0.9,
     marginTop: Metrics.ratio(20),
+  },
+
+  codeFieldRoot: {
+    marginTop: Metrics.ratio(20),
+    width: Metrics.screenWidth * 0.4,
+    alignItems: 'center'
+  },
+
+
+  defaultCellContainer: {
+    backgroundColor: Colors.lightMode.grey8,
+    borderRadius: Metrics.ratio(20),
+    height: Metrics.screenHeight * 0.015,
+    width: Metrics.screenHeight * 0.015,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+
+  containerCellView: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: Colors.lightMode.white,
+  },
+
+  focusCell: {
+    color: Colors.lightMode.black4,
+    fontSize: Metrics.ratio(20),
+    fontWeight: '700',
+    alignSelf: 'center',
   },
 });
 
